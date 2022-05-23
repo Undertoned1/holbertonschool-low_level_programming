4719b4f655873aa734c36c4868808c31bb3c53bc
Undertoned1 <Undertoned1@gmail.com>
2022-05-23 10:49:34 -0700

    betty for 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-  char x;
+char x;
 
-  for (x = 'a'; x <= 'z'; ++x)
-    putchar(x);
-  putchar('\n');
+for (x = 'a'; x <= 'z'; ++x)
+putchar(x);
+putchar('\n');
 
-  return (0);
+return (0);
 }
