f819cffb7627c94bb823222d2d80b1dd1e5b5896
Undertoned1 <Undertoned1@gmail.com>
2022-05-25 08:26:17 -0700

    testing 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+
+{
+int x = 1;
+
+while (x < 10)
+{
+	printf("%d", x);
+	x++;
+}
+putchar('\n');
+return (0);
+}
