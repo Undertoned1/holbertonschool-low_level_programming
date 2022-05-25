008a6d873f253ea81421aa91fdbda466d5b626cd
Undertoned1 <Undertoned1@gmail.com>
2022-05-25 12:04:39 -0700

    uploading 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+
+{
+	int x = 48;
+
+	while (x <= 57)
+	{
+		putchar(x);
+		if (x != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
