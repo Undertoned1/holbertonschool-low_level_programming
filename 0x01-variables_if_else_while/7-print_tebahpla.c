d3113c12323c271159da8729b0158e7916aa43e7
Undertoned1 <Undertoned1@gmail.com>
2022-05-25 09:38:22 -0700

    thank you

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+
+{
+char x = 'z';
+while (x >= 'a')
+{
+	putchar(x);
+	x--;
+}
+putchar('\n');
+return (0);
+}
